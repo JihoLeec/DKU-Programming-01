1861f812936b70143cccd7a0b2fd7de45405586d
Jiho Lee <ljh32233586@dankook.ac.kr>
2025-04-01 09:50:21 +0900

    lecture 6,7
    Operator, Condition(If, Switch)

---
+++ src/lecture6/Complement.java
@@ -0,0 +1,20 @@
+package lecture6;
+
+public class Complement {
+    public static void main(String[] args) {
+        // 2의 보수법
+        // 0000 0101 -> 5
+        // 1111 1010 모든 부호 반전
+        // 1111 1011 -> -5. +1을 해준다.
+
+        int a = 5,b=3; // 0101,0011
+        System.out.println(Integer.toBinaryString(a)); // 101(앞에 0붙는건 다 삭제됨)
+        System.out.println(Integer.toBinaryString(b)); // 11
+
+        // 비트 연산자
+        System.out.println(a&b); // 1 <- 0001
+        System.out.println(a|b); // 7 <- 0111
+        System.out.println(a^b); // 6 <- 0110
+        System.out.println(~a); // -6 <- 1010(-1 하면 1001 -> 0110)
+    }
+}
---
+++ src/lecture7/Condition01.java
@@ -0,0 +1,27 @@
+package lecture7;
+
+public class Condition01 {
+    public static void main(String[] args) {
+        /**
+         * if(조건식) {
+         *     어쩌고저쩌고
+         * }
+         */
+        int number=7;
+
+        if(number%2!=0) {
+            System.out.println("홀수입니다.");
+        } else {
+            System.out.println("짝수입니다.");
+        }
+
+        int age = 68;
+        boolean isStudent = false;
+        boolean isVeteran = true;
+
+        if (isStudent || age >= 65 || isVeteran) {
+            System.out.println("할인 대상입니다.");
+        }
+        // 놀라운 사실: c처럼 int 값을 자동으로 boolean으로 변환해주지 않는다.
+    }
+}
