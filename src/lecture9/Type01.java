d065f09a5fd4382165c0236f6885968b4b1b9343
Jiho Lee <159555244+JihoLeec@users.noreply.github.com>
2025-04-12 21:28:27 +0900

    Update Type01.java

--- src/lecture9/Type01.java
+++ src/lecture9/Type01.java
@@ -11,8 +11,8 @@ public class Type01 {
         String str3 = new String("안녕");
         String str4 = new String("안녕");
 
-        System.out.println(str1==str2); //true; 리터럴로 생성. 이때는 값이 같기에 같은 주소를 가르킴 
-                                        //스택 영역에서는 다른 객체이나 힙에서는 같은 문자열을 가르킴
+        System.out.println(str1==str2); //true; 리터럴로 생성. 이때는 값이 같기에 같은 주소를 가리킴 
+                                        //스택 영역에서는 다른 객체이나 힙에서는 같은 문자열을 가리킴
         System.out.println(str1.equals(str2)); //true;  값이 안녕으로 같음.
         System.out.println(str3==str4); //false;    주소가 다르기 때문임
         System.out.println(str3.equals(str4)); //true; 값이 안녕으로 같음
